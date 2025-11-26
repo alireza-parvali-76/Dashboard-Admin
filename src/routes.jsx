38cf8e64830dcd78d420b4d1fa20e6812f729be8
alireza-parvali-76 <alirezaparvali76@gmail.com>
2025-11-26 12:20:08 +0330

    Finaly Change

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,11 @@
 import Home from "./pages/home/Home"
 import UserList from "./pages/Users/UserList"
-import NewUser from "./pages/newUser/NewUser"
 import Product from "./pages/products/Products"
 import Products from "./pages/Product/Product"
 
 let routes = [
     {path:'/' , element: <Home />},
     {path: '/users', element: <UserList />},
-    {path: '/newUser', element: <NewUser />},
     {path: '/product', element: <Product />},
     {path: '/product/:productID', element: <Products />},
 ]
